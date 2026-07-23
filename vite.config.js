b960578bd3a7743b410f637bb5c6009a7d41a07a
Asliddin <asliddin6667@gmail.com>
2026-07-23 08:46:54 +0500

    Sub-path rejimiga qaytarish

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
-import { defineConfig } from 'vite'
+﻿import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // GitHub Pages sub-path rejimi: asliddin6667-lab.github.io/edujadval/
-  // KELAJAKDA custom domen (edujadval.uz) ulasangiz: base: '/' qiling
-  // va public/CNAME faylini qayta yarating (ichida: edujadval.uz)
   base: '/edujadval/',
   server: {
     port: 5175,
